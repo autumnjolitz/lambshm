d105afd8efa3fb0730e630cc370c14203930f60b
Autumn Jolitz <270476+autumnjolitz@users.noreply.github.com>
2023-06-22 17:08:21 -0700

    [*] replace upload script, use ``invoke`` as the effective Makefile for everything
    - [upload-to-github] remove as unused
    - [docker-image] let ``tasks.py`` deduce the upload
    - [tasks] attempt to deduce from environment the GHCR owner and version
    - [upload-to-github] add some debugging
    - [tasks] fix upload to use internal Context-less function
    - [tasks] use our friendly name
    - [tasks] dereference the sha -> our name
    - [tasks] cache un-sha-tagged images by default
    - [tasksupport] try to preserve internal function signatures, look at ``task.py`` for formatting defaults
    - [.gitignore] ignore the sublime workspace
    - [tasks] add some safeties in building
    - [tasksupport] add trim functions of string
    - [.sublime-project] add project configuration
    - [docker-image, tasks] fix
    - [tasksupport] add missing import
    - [upload-to-github, docker-image] update workflow
    - [tasks] clean up, add more task functions
    - [tasksupport] make it easier to codegen, allow ``locals`` to chainmap more appropriately

--- tasks.py
+++ tasks.py
@@ -2,29 +2,61 @@ import json
 import os
 import shutil
 import types
-import pprint
+import base64
 import datetime
 import builtins
 import sys
+import time
+from contextlib import suppress, contextmanager
 from invoke.exceptions import UnexpectedExit
-from typing import Literal, Type, Union, Iterable, Dict, Tuple, NamedTuple
-
 from pathlib import Path
-from invoke.context import Context
-from contextlib import suppress
+from typing import Literal, Type, Union, Iterable, Dict, Tuple, NamedTuple, Optional, List
 
-from tasksupport import task, first
+from invoke.context import Context
+from tasksupport import task, first, InvertedMapping, trim
 
 _ = types.SimpleNamespace()
 this = sys.modules[__name__]
 AWS_LAMBDA_REPO = "public.ecr.aws/lambda/python"
-BASE_IMAGES = {
+BASE_IMAGES: dict[str, str | None] = {
     # python:3.8
-    AWS_LAMBDA_REPO: f"{AWS_LAMBDA_REPO}@sha256:a04abc05330a09c239c3e3d62408dd8331c5b3e3ee323a3d8a29cb0fad4d5356",
+    f"{AWS_LAMBDA_REPO}:3.8": f"{AWS_LAMBDA_REPO}@sha256:a04abc05330a09c239c3e3d62408dd8331c5b3e3ee323a3d8a29cb0fad4d5356",
+    # python:3.9
+    # f"{AWS_LAMBDA_REPO}:3.9": f"{AWS_LAMBDA_REPO}@sha256:24c5f5135c69f00ff9e43c320b7602177f390ed6637ac00f07272e812a286dc4",
 }
+BASE_IMAGES_BY_SHA = InvertedMapping(BASE_IMAGES)
+IMAGE_DIGEST_CACHE_TTL: int | float = 5 * 60
+EMPTY_MAPPING = {}
+DEFAULT_FORMAT = "lines"
 
 
-_EMPTY_MAPPING = {}
+def _task_init():
+    del globals()["_task_init"]
+    root = _.project_root(Path, silent=True)
+    for image_tag in BASE_IMAGES:
+        override_filename = root / f".overrides.{_.b64encode(image_tag)}"
+        if not override_filename.exists():
+            continue
+        if BASE_IMAGES[image_tag] is not None:
+            with suppress(FileNotFoundError):
+                os.remove(override_filename)
+            continue
+        with suppress(FileNotFoundError):
+            with open(override_filename, "r") as fh:
+                image_sha, ts = fh.read().strip().splitlines()
+                ts = float(ts)
+                ttl = ts - time.time()
+                if ttl > 0:
+                    print(
+                        f"Loaded cached SHA ({image_sha!r}) for {image_tag!r} (TTL {ttl:.2f}). Please add a SHA to the BASE_IMAGES!",
+                        file=sys.stderr,
+                    )
+                    BASE_IMAGES[image_tag] = image_sha
+                    continue
+                print(
+                    f"Ignoring cached {image_sha!r} -- expired {ttl} seconds ago", file=sys.stderr
+                )
+                os.remove(override_filename)
 
 
 class HashedImage(NamedTuple):
@@ -33,12 +65,17 @@ class HashedImage(NamedTuple):
     hash: str
 
 
+class Image(NamedTuple):
+    name: str
+    tags: Tuple[str, ...]
+
+
 def compose_environ(*, copy_os_environ: bool = False, **kwargs) -> Dict[str, str]:
     """
     Returns some common values for Docker builds
     """
     environment = {
-        **(os.environ if copy_os_environ else _EMPTY_MAPPING),
+        **(os.environ if copy_os_environ else EMPTY_MAPPING),
         "NO_COLOR": "1",
         "COMPOSE_DOCKER_CLI_BUILD": "1",
         "BUILDX_EXPERIMENTAL": "1",
@@ -48,6 +85,49 @@ def compose_environ(*, copy_os_environ: bool = False, **kwargs) -> Dict[str, str
         **kwargs,
     }
     return environment
+
+
+@contextmanager
+def cd(path: str | Path):
+    if not isinstance(path, Path):
+        path = Path(path)
+    new_cwd = path.resolve()
+    prior_cwd = Path(os.getcwd()).resolve()
+    try:
+        os.chdir(new_cwd)
+        yield prior_cwd
+    finally:
+        os.chdir(prior_cwd)
+
+
+@task
+def branch_name(context: Context) -> str:
+    with suppress(KeyError):
+        return os.environ["GITHUB_REF_NAME"]
+    here = this._.project_root(Path, silent=True)
+    if (here / ".git").is_dir():
+        with suppress(FileNotFoundError):
+            return context.run(f"git -C {here!s} branch --show-current", hide="both").stdout.strip()
+        with open(here / ".git" / "HEAD") as fh:
+            for line in fh:
+                if line.startswith("ref:"):
+                    _, line = (x.strip() for x in line.split(":", 1))
+                if line.startswith("refs/heads/"):
+                    return line.removeprefix("refs/heads/")
+    raise ValueError("Unable to determine branch name!")
+
+
+@task
+def b64encode(value: str, silent: bool = True) -> str:
+    return base64.urlsafe_b64encode(value.encode()).decode().strip()
+
+
+@task
+def b64decode(value: str, silent: bool = True) -> str:
+    remainder = len(value) % 8
+    if remainder:
+        value += "=" * remainder
+    return base64.urlsafe_b64decode(value).decode().strip()
 
 
 @task
@@ -229,7 +309,7 @@ def all_source_image_names(context, silent: bool = False) -> Tuple[str, ...]:
     List the source image friendly names
     """
     all_images = []
-    for base_image in BASE_IMAGES.values():
+    for base_image in BASE_IMAGES_BY_SHA:
         with suppress(ValueError):
             image, hash_function, value = _.split_image_hash(context, base_image)
             if not silent:
@@ -244,22 +324,107 @@ def all_source_image_names(context, silent: bool = False) -> Tuple[str, ...]:
 
 
 @task
-def download(context: Context):
-    for key, value in BASE_IMAGES.items():
-        context.run(f"docker pull {value}", env=compose_environ())
-        context.run(f"docker tag {value} {key}")
+def digests_for(context: Context, image_name: str, *, silent: bool = False) -> tuple[str, ...]:
+    image_name_prefix = image_name
+    if ":" in image_name_prefix:
+        image_name_prefix, _ = image_name_prefix.split(":", 1)
+    fh = context.run(
+        "docker inspect --format='{{.RepoDigests}}' %s" % (image_name,),
+        hide="both" if silent else None,
+    )
+    digests = trim(fh.stdout.strip(), "[]").split(" ")
+    if not silent:
+        print(f"Digests for {image_name}: {', '.join(digests)}")
+    return tuple(x for x in digests if x.startswith(image_name_prefix))
 
 
 @task
-def image_name(context: Context, base_image: str) -> str:
+def download(context: Context, /, silent: bool = False) -> Tuple[Image, ...]:
+    downloaded: List[Image] = []
+    for image_sha in BASE_IMAGES_BY_SHA:
+        if image_sha is None:
+            continue
+        context.run(
+            f"docker pull {image_sha}", env=compose_environ(), hide="both" if silent else None
+        )
+        tags = BASE_IMAGES_BY_SHA[image_sha:image_sha]
+        for tag in tags:
+            if not silent:
+                print(f"Tagging {image_sha} -> {tag}")
+            context.run(f"docker tag {image_sha} {tag}", hide="both" if silent else None)
+        downloaded.append(Image(image_sha, tuple(tags)))
+    for image_tag in BASE_IMAGES:
+        if BASE_IMAGES[image_tag] is None:
+            context.run(
+                f"docker pull {image_tag}", env=compose_environ(), hide="both" if silent else None
+            )
+            image_sha = _.cached_digest_for(context, image_tag, silent=True)
+            if not silent:
+                print(f"Assigning {image_sha} to {image_tag} for this run", file=sys.stderr)
+            BASE_IMAGES[image_tag] = image_sha
+    return tuple(downloaded)
+
+
+@task
+def cached_digest_for(
+    context: Context,
+    image_tag: str,
+    expires_in: float | int = IMAGE_DIGEST_CACHE_TTL,
+    silent: bool = False,
+) -> str:
+    root = _.project_root(Path, silent=True)
+    file = root / f".overrides.{_.b64encode(image_tag)}"
+    with suppress(FileNotFoundError):
+        with open(file) as fh:
+            image_sha, t_s = fh.read().strip().splitlines()
+            t_s = float(t_s)
+            ttl = t_s - time.time()
+            if ttl < expires_in:
+                return image_sha
+            # expired!
+            if not silent:
+                print(f"Removing {file!r}, renewing digests!", file=sys.stderr)
+            os.remove(file)
+    (image_sha,) = _.digests_for(context, image_tag, silent=True)
+    with open(file, "w") as fh:
+        fh.write(f"{image_sha}\n{time.time() + expires_in!s}\n")
+    return image_sha
+
+
+@task()
+def our_image_name_for(
+    context: Context,
+    /,
+    base_image: Optional[str] = None,
+    skip_tag: List[str] = ["latest", "head", "main", "master"],
+    all: bool = False,
+) -> str:
     """
     Given a base image, return the expected patched output name
     """
-    (base_image, *_) = this._.get_tags_from(context, base_image, silent=True)
-    _, image = base_image.rsplit("/", 1)
-    image = image.translate({ord(":"): None})
-    image = f"lambshm/{image}"
-    return image
+    if base_image is None:
+        base_image = first(BASE_IMAGES_BY_SHA)
+    image_tags = this._.get_tags_from(context, base_image, silent=True)
+    results = []
+    skip_tag = frozenset(skip_tag)
+    for base_image in image_tags:
+        _, image = base_image.rsplit("/", 1)
+        tag_name = ""
+        with suppress(ValueError):
+            image, tag_name = image.split(":")
+            if tag_name in skip_tag:
+                continue
+        if tag_name:
+            if tag_name[0].isdigit():
+                image = f"lambshm/{image}{tag_name}"
+            else:
+                image = f"lambshm/{image}-{tag_name}"
+        if not all:
+            return image
+        results.append(image)
+    if not all and not results:
+        raise FileNotFoundError(f"Unable to find images for {base_image}")
+    return tuple(results)
 
 
 @task
@@ -270,7 +435,7 @@ def all_image_names(
     List all the expected image names given the BASE_IMAGES
     """
     images = []
-    for base_image in BASE_IMAGES.values():
+    for base_image in BASE_IMAGES_BY_SHA:
         images.append(image_name(context, base_image, silent=True))
     return tuple(images)
 
@@ -284,20 +449,25 @@ def build(
     """
     now = datetime.datetime.utcnow().astimezone(datetime.timezone.utc).isoformat(timespec="seconds")
     images = []
-    for base_image in BASE_IMAGES.values():
-        image_name = _.image_name(context, base_image, silent=True)
+    for base_image in BASE_IMAGES_BY_SHA:
+        image_name = _.our_image_name_for(context, base_image, silent=True)
         if runtime:
             if not silent:
                 print("Building runtime image", file=sys.stderr)
-            context.run(
+            path = (
                 "docker compose --ansi never "
                 "-f config/docker-compose.yml "
                 "build "
                 f"--build-arg BASE_IMAGE={base_image} "
                 f"--build-arg TODAY={now} "
-                "runtime",
+                "runtime"
+            )
+            if not silent:
+                print(f"Running {path!r} {image_name!r}", file=sys.stderr)
+            context.run(
+                path,
                 env=compose_environ(IMAGE_NAME=image_name),
-                hide="both" if silent else None,
+                hide=("both" if silent else None),
             )
             images.append(image_name)
         if tests:
@@ -311,7 +481,7 @@ def build(
                 f"--build-arg TODAY={now} "
                 "runtime",
                 env=compose_environ(IMAGE_NAME=image_name),
-                hide="both" if silent else None,
+                hide=("both" if silent else None),
             )
             images.append(f"{image_name}-test")
     return tuple(images)
@@ -324,41 +494,127 @@ def test(context: Context, as_server: bool = False, silent: bool = False) -> boo
 
     returns if it passes the test
     """
-    image_name = _.image_name(context, first(BASE_IMAGES.values()), silent=True)
-    env = compose_environ(IMAGE_NAME=image_name)
-    if as_server:
-        result = context.run(
-            "docker compose --ansi never "
-            "-f config/docker-compose.yml -f config/docker-compose.test.yml "
-            "run --rm "
-            "runtime ",
-            env=env,
-            hide="both" if silent else None,
-        )
-    else:
-        result = context.run(
-            "docker compose --ansi never "
-            "-f config/docker-compose.yml -f config/docker-compose.test.yml "
-            "run --rm --entrypoint /bin/sh "
-            "runtime "
-            "-c 'mkdir /tmp/shm && python lambda_handler.py'",
-            env=env,
-            hide="both" if silent else None,
-        )
-    if result:
-        return True
-    return False
+    for image_sha in BASE_IMAGES_BY_SHA:
+        image_name = _.our_image_name_for(context, image_sha, silent=True)
+        env = compose_environ(IMAGE_NAME=image_name)
+        if as_server:
+            result = context.run(
+                "docker compose --ansi never "
+                "-f config/docker-compose.yml -f config/docker-compose.test.yml "
+                "run --rm "
+                "runtime ",
+                env=env,
+                hide="both" if silent else None,
+            )
+        else:
+            result = context.run(
+                "docker compose --ansi never "
+                "-f config/docker-compose.yml -f config/docker-compose.test.yml "
+                "run --rm --entrypoint /bin/sh "
+                "runtime "
+                "-c 'mkdir /tmp/shm && python lambda_handler.py'",
+                env=env,
+                hide="both" if silent else None,
+            )
+        if not result:
+            return False
+    return True
 
 
 @task
-def list_local_images(context: Context) -> Tuple[str, ...]:
+def list_local_images(
+    context: Context, /, show: Literal["test", "runtime", "both"] = "both"
+) -> Tuple[str, ...]:
+    if show not in ("test", "runtime", "both"):
+        raise SystemExit(f'Invalid show mode {show!r} - try one of {{"test", "both", "runtime"}} ')
+    images = []
     result = context.run("docker image ls --format '{{ .Repository}}' lambshm/*", hide="both")
-    image_ids = [x.strip() for x in result.stdout.splitlines() if x.strip()]
-    return tuple(image_ids)
+    for image in (x.strip() for x in result.stdout.splitlines() if x.strip()):
+        if image.endswith("-test"):
+            if show in ("both", "test"):
+                images.append(image)
+            continue
+        if show in ("both", "runtime"):
+            images.append(image)
+    return tuple(images)
 
 
 @task
-def list_containers_using(context: Context, image_id: str, silent: bool = False) -> Tuple[str, ...]:
+def repository_owner(context: Context, /) -> str | None:
+    owner = None
+    with suppress(KeyError):
+        owner = os.environ["GITHUB_ACTOR"]
+    if not owner:
+        with cd(this._.project_root(silent=True)):
+            git_repo_url = context.run("git remote get-url origin", hide="stdout")
+            if git_repo_url:
+                _, short_repo_url = git_repo_url.stdout.split(":", 1)
+                owner, _ = short_repo_url.split("/", 1)
+    return owner
+
+
+@task
+def upload(context: Context, /, build_ref: str = "", silent: bool = False, owner: str = "") -> None:
+    if not build_ref:
+        with suppress(KeyError):
+            build_ref = os.environ["GITHUB_REF"]
+    if not build_ref:
+        build_ref = f"refs/heads/{_.branch_name(context)}"
+        print(f"build_ref inferred from branch_name to be {build_ref}", file=sys.stderr)
+    if not owner:
+        owner = _.repository_owner(context, silent=True)
+        print(f"owner inferred to be {owner!r}")
+    if not owner:
+        raise ValueError("Unable to determine owner of repository, please specify an owner!")
+    images = list_local_images(context, "runtime")
+    if build_ref.startswith("refs/tags/"):
+        tag_name = build_ref.removeprefix("refs/tags/").translate({ord("/"): ord("-")})
+        assert tag_name.lower() not in ("main", "latest", "master")
+        version = tag_name
+    elif build_ref.startswith("refs/heads/"):
+        branch_name = build_ref.removeprefix("refs/heads/").translate({ord("/"): ord("-")})
+        if branch_name == "main":
+            version = "latest"
+        else:
+            version = f"branch.{branch_name}"
+    elif build_ref.startswith("refs/pull/"):
+        print("Nothing to upload for pull requests!", file=sys.stderr)
+        return
+    else:
+        raise ValueError(f"Unrecognized build_ref {build_ref!r}")
+    if images:
+        t_s = time.time()
+        print(f"Begin upload of {len(images)} images")
+        for image in images:
+            _.upload_image(context, image, version, silent=silent, owner=owner)
+        print(f"Uploaded {len(images)} images in {time.time() - t_s:.2f} seconds")
+
+
+@task
+def upload_image(
+    context: Context, /, image: str, version: str, owner: str = "", silent: bool = False
+) -> None:
+    if not owner:
+        owner = _.repository_owner(context, silent=True)
+    if not owner:
+        raise ValueError("Unable to determine owner of repository, please specify an owner!")
+    image_prefix = f"ghcr.io/{owner}/{{}}:{version}"
+    image_repository = image_prefix.format(image)
+    if not silent:
+        print(f"Uploading {image} to {image_repository}", file=sys.stderr)
+    t_s = time.time()
+    context.run(f"docker tag {image} {image_repository}", hide="both" if silent else None)
+    context.run(f"docker push {image_repository}", hide="both" if silent else None)
+    if not silent:
+        print(
+            f"Uploaded {image} to {image_repository} in {time.time() - t_s:.2f}s", file=sys.stderr
+        )
+
+
+@task
+def list_containers_using(
+    context: Context, /, image_id: str, silent: bool = False
+) -> Tuple[str, ...]:
     format = '--format "{{.ID}}"'
     result = context.run(
         f"docker container ls --all --filter=ancestor='{image_id}' {format}", hide="both"
@@ -383,3 +639,6 @@ def clean(context: Context, silent: bool = False) -> None:
         image_ids = " ".join(image_ids)
         context.run(f"docker rmi -f {image_ids}", hide="both")
     context.run("docker builder prune -f", hide="both")
+
+
+_task_init()
